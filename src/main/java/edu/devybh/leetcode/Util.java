f5e2193ac9987a66d0d43a93c488ffa051651cd7
유병화 <devybh@mt.co.kr>
2020-04-10 09:47:14 +0900

    로그 기능 개선

--- src/main/java/edu/devybh/leetcode/Util.java
+++ src/main/java/edu/devybh/leetcode/Util.java
@@ -5,43 +5,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Util {
     public static void log(String log) {
-        String forPrint = "";
-
-        // trace
-        StackTraceElement[] stackTraceElements = new Throwable().getStackTrace();
-        String caller = stackTraceElements[1].getClassName();
-        caller = caller.substring(caller.lastIndexOf(".") + 1);
-        forPrint = "[" + caller.substring(caller.lastIndexOf(".") + 1) + "]";
-
-        // data to string
-        forPrint += " " +log;
-
-        // print
+        String forPrint = log + " (" + getStackTraceText() + ")\n";
         System.out.println(forPrint);
-
     }
 
     public static void log(String title, Object obj) {
-        String forPrint = "";
-
-        // trace
-        StackTraceElement[] stackTraceElements = new Throwable().getStackTrace();
-        String caller = stackTraceElements[1].getClassName();
-        caller = caller.substring(caller.lastIndexOf(".") + 1);
-        forPrint = "[" + caller.substring(caller.lastIndexOf(".") + 1) + "]";
-
-        // title
-        forPrint += " " + title + "\n";
-
+        String forPrint = "[" + title + "]";
+        forPrint += "\n  trace: " + getStackTraceText();
+        forPrint += "\n  type: " + obj.getClass().getTypeName();
         // data to string
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            forPrint += " " + objectMapper.writeValueAsString(obj);
+            forPrint += "\n  value: " + objectMapper.writeValueAsString(obj) + "\n";
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
         // print
         System.out.println(forPrint);
+    }
+
+    private static String getStackTraceText() {
+        // stackTrace
+        StackTraceElement[] stackTraceElements = new Throwable().getStackTrace();
+        // make stackTrace title
+        String className = stackTraceElements[2].getClassName();
+        className = className.substring(className.lastIndexOf(".") + 1);
+        String methodName = stackTraceElements[2].getMethodName();
+        String lineNumber = String.valueOf(stackTraceElements[2].getLineNumber());
+        // for Print
+        return className + "." + methodName + "():" + lineNumber;
     }
 }
